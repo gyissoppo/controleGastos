bc5f979bfa6947c71c894e9873baf9e85b7a6930
gyovana.issoppo <gyovana.issoppo@gmail.com>
2022-07-07 21:26:12 -0300

    comentario :)

--- tabelagastos.sql
+++ tabelagastos.sql
@@ -1,18 +1,18 @@
-create database tabelagastos;
-use tabelagastos;
+create database tabelagastos; 
+use tabelagastos; 
 
-create table mes(
-codigo int primary key auto_increment not null,
-mesAno varchar(100) not null,
-ganho varchar(100) not null
-) Engine = InnoDB;
+create table mes( 
+codigo int primary key auto_increment not null, 
+mesAno varchar(100) not null, 
+ganho varchar(100) not null 
+) Engine = InnoDB; 
 
-create table gastos(
-codigo int primary key auto_increment not null,
-dia int not null,
-periodo varchar(100) not null,
-item varchar(150) not null,
-valor varchar(100) not null,
-onde varchar(100) not null,
-forma varchar(100) not null
+create table gastos( 
+codigo int primary key auto_increment not null, 
+dia int not null, 
+periodo varchar(100) not null, 
+item varchar(150) not null, 
+valor varchar(100) not null, 
+onde varchar(100) not null, 
+forma varchar(100) not null 
 ) Engine = InnoDB;
